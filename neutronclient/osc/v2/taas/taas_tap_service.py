c48b946ea2426668e80d9c1455af2a767408fa25
Deepak Tiwari <deepak.dt@gmail.com>
2018-08-14 19:38:31 +0530

    Update taas_tap_service.py

--- neutronclient/osc/v2/taas/taas_tap_service.py
+++ neutronclient/osc/v2/taas/taas_tap_service.py
@@ -18,9 +18,10 @@ import logging
 from osc_lib.command import command
 from osc_lib import exceptions
 from osc_lib import utils
-from osc_lib.utils import columns as column_util
+#from osc_lib.utils import columns as column_util
 
 from neutronclient._i18n import _
+from neutronclient.osc import utils as column_util
 
 LOG = logging.getLogger(__name__)
 
